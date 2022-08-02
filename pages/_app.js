35aa6005761d6334d4ee0526d9d6473d2b53c239
liorheber <lior.heber@kenshoo.com>
2022-08-02 19:46:36 -0400

    add mixpanel integration option

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import "../styles/globals.css";
 
+const MixpanelTracking =
+  process.env.NEXT_PUBLIC_MIXPANEL_TOKEN &&
+  require("../integrations/mixpanel").default;
+
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="h-full bg-gray-100 min-h-screen max-w-screen overflow-x-clip">
-      <Component {...pageProps} />
-    </div>
+    <>
+      {MixpanelTracking ? <MixpanelTracking /> : null}
+      <div className="h-full bg-gray-100 min-h-screen max-w-screen overflow-x-clip">
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
 
