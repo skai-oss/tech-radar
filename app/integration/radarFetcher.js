080f208dd965b91440cd87a2ca2133c2c473a1d7
liorheber <lior.heber@kenshoo.com>
2023-04-09 18:35:35 -0500

    attempt to fetch csv dynamically

--- app/integration/radarFetcher.js
+++ app/integration/radarFetcher.js
@@ -2,8 +2,8 @@ import csvToJson from "csvtojson";
 import path from "node:path";
 import { prepareGraphData } from "./utils/graph_utils";
 
-export const fetchRadar = async (slug) => {
-  const fileName = slug ? `${slug}.csv` : "latest.csv";
+export const fetchRadar = async (slug = "latest") => {
+  const fileName = `${slug}.csv`;
   const filePath = path.join(process.cwd(), "data", fileName);
   const csvData = await csvToJson().fromFile(filePath);
   return prepareGraphData(csvData);
