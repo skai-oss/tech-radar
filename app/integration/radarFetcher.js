feea282edb04cc0819694018a7c8981b9d57c072
liorheber <lior.heber@kenshoo.com>
2023-04-09 18:26:48 -0500

    Add new data

--- app/integration/radarFetcher.js
+++ app/integration/radarFetcher.js
@@ -2,8 +2,9 @@ import csvToJson from "csvtojson";
 import path from "node:path";
 import { prepareGraphData } from "./utils/graph_utils";
 
-export const fetchRadar = async () => {
-  const filePath = path.join(process.cwd(), "data", "data.csv");
+export const fetchRadar = async (slug) => {
+  const fileName = slug ? `${slug}.csv` : "latest.csv";
+  const filePath = path.join(process.cwd(), "data", fileName);
   const csvData = await csvToJson().fromFile(filePath);
   return prepareGraphData(csvData);
 };
