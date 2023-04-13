d37ec2e105cb4161858e8260522bfdfe2d903392
Lior Heber <lior.heber@kenshoo.com>
2023-04-13 13:57:23 -0500

    attempt to fetch csv dynamically (#12)
    * attempt to fetch csv dynamically
    
    * attempt to fetch csv dynamically
    
    * remove staticness
    
    * refactor chart logic
    
    * revalidate within the hour

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <link rel="icon" href="/public/favicon.ico" />
       <body className="h-full bg-gray-100 min-h-screen max-w-screen overflow-x-clip">
         <main className="py-10 w-full">
           <Header />
